211122d775cd59dc5f7c06669e66ee00ad5aa209
Milan Singh <milan083821@gmail.com>
2025-04-18 15:08:43 +0530

    add logout menu to avatar

--- src/components/shared/AppSidebar.tsx
+++ src/components/shared/AppSidebar.tsx
@@ -1,4 +1,10 @@
-import { CircleUserRound, Home, TvMinimalPlay, History, Settings } from "lucide-react";
+import {
+  CircleUserRound,
+  Home,
+  TvMinimalPlay,
+  History,
+  Settings,
+} from "lucide-react";
 import { NavLink } from "react-router-dom";
 import { useSidebar } from "@/components/ui/sidebar";
 import {
@@ -10,33 +16,51 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { UseAuth } from "@/context/AuthProvider";
 
 export function AppSidebar() {
-    const {open} = useSidebar()
+  const { open } = useSidebar();
+  const { user } = UseAuth();
   return (
-    <Sidebar collapsible="icon" variant="sidebar" className="h-[90vh] top-20 left-3 border-none transition-none">
+    <Sidebar
+      collapsible="icon"
+      variant="sidebar"
+      className="h-[90vh] top-20 left-3 border-none transition-none"
+    >
       <SidebarContent className="bg-black text-white p-0">
         {/* Group 1: Home & Subscriptions */}
-        <SidebarGroup className={`${open ? "border-b border-gray-800" : "" }`}>
+        <SidebarGroup className={`${open ? "border-b border-gray-800" : ""}`}>
           <SidebarGroupContent>
             <SidebarMenu>
               <SidebarMenuItem>
                 <SidebarMenuButton asChild className="my-1">
-                  <NavLink to="/" className={({ isActive }) => 
-                    isActive ? "text-white font-medium" : ""
-                  }>
-                    <span><Home /></span>
+                  <NavLink
+                    to="/"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-medium" : ""
+                    }
+                  >
+                    <span>
+                      <Home />
+                    </span>
                     <span className="md:text-lg font-normal ml-3">Home</span>
                   </NavLink>
                 </SidebarMenuButton>
               </SidebarMenuItem>
               <SidebarMenuItem>
                 <SidebarMenuButton asChild className="my-1">
-                  <NavLink to="/subscriptions" className={({ isActive }) => 
-                    isActive ? "text-white font-medium" : ""
-                  }>
-                    <span><TvMinimalPlay /></span>
-                    <span className="md:text-lg font-normal ml-3">Subscriptions</span>
+                  <NavLink
+                    to="/subscriptions"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-medium" : ""
+                    }
+                  >
+                    <span>
+                      <TvMinimalPlay />
+                    </span>
+                    <span className="md:text-lg font-normal ml-3">
+                      Subscriptions
+                    </span>
                   </NavLink>
                 </SidebarMenuButton>
               </SidebarMenuItem>
@@ -50,20 +74,30 @@ export function AppSidebar() {
             <SidebarMenu>
               <SidebarMenuItem>
                 <SidebarMenuButton asChild className="my-1">
-                  <NavLink to="/you" className={({ isActive }) => 
-                    isActive ? "text-white font-medium" : ""
-                  }>
-                    <span><CircleUserRound /></span>
+                  <NavLink
+                    to="/you"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-medium" : ""
+                    }
+                  >
+                    <span>
+                      <CircleUserRound />
+                    </span>
                     <span className="md:text-lg font-normal ml-3">You</span>
                   </NavLink>
                 </SidebarMenuButton>
               </SidebarMenuItem>
               <SidebarMenuItem>
                 <SidebarMenuButton asChild className="my-1">
-                  <NavLink to="/history" className={({ isActive }) => 
-                    isActive ? "text-white font-medium" : ""
-                  }>
-                    <span><History /></span>
+                  <NavLink
+                    to="/history"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-medium" : ""
+                    }
+                  >
+                    <span>
+                      <History />
+                    </span>
                     <span className="md:text-lg font-normal ml-3">History</span>
                   </NavLink>
                 </SidebarMenuButton>
@@ -73,18 +107,22 @@ export function AppSidebar() {
         </SidebarGroup>
 
         {/* Group 3: Sign In Prompt */}
-        <SidebarGroup className={`${open ? "block" : "hidden" }`}>
-          <SidebarGroupContent>
-            <div className="px-4 py-6 border-t border-b border-gray-800">
-              <p className="text-sm mb-3">Sign in to like videos, comment and subscribe.</p>
-              <NavLink to="/signin">
-                <button className="w-full py-1.5 px-4 rounded-full border border-blue-500 text-blue-500 font-medium hover:bg-primary hover:text-white hover:border-white hover:cursor-pointer hover:bg-opacity-10 transition">
-                  Log in
-                </button>
-              </NavLink>
-            </div>
-          </SidebarGroupContent>
-        </SidebarGroup>
+        {!user && (
+          <SidebarGroup className={`${open ? "block" : "hidden"}`}>
+            <SidebarGroupContent>
+              <div className="px-4 py-6 border-t border-b border-gray-800">
+                <p className="text-sm mb-3">
+                  Sign in to like videos, comment and subscribe.
+                </p>
+                <NavLink to="/login">
+                  <button className="w-full py-1.5 px-4 rounded-full border border-blue-500 text-blue-500 font-medium hover:bg-primary hover:text-white hover:border-white hover:cursor-pointer hover:bg-opacity-10 transition">
+                    Log in
+                  </button>
+                </NavLink>
+              </div>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        )}
 
         {/* Group 4: Settings */}
         <SidebarGroup>
@@ -92,11 +130,18 @@ export function AppSidebar() {
             <SidebarMenu>
               <SidebarMenuItem>
                 <SidebarMenuButton asChild className="my-1">
-                  <NavLink to="/settings" className={({ isActive }) => 
-                    isActive ? "text-white font-medium" : ""
-                  }>
-                    <span><Settings /></span>
-                    <span className="md:text-lg font-normal ml-3">Settings</span>
+                  <NavLink
+                    to="/settings"
+                    className={({ isActive }) =>
+                      isActive ? "text-white font-medium" : ""
+                    }
+                  >
+                    <span>
+                      <Settings />
+                    </span>
+                    <span className="md:text-lg font-normal ml-3">
+                      Settings
+                    </span>
                   </NavLink>
                 </SidebarMenuButton>
               </SidebarMenuItem>
--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -1,17 +1,35 @@
 import { Button } from "@/components/ui/button";
 import Logo from "./Logo";
-import { CircleUserRound } from "lucide-react";
+import { CircleUserRound, LayoutDashboard, User, Power } from "lucide-react";
 import { useSidebar } from "@/components/ui/sidebar";
 import { Menu } from "lucide-react";
 import { UseAuth } from "@/context/AuthProvider";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { DropdownMenuGroup } from "@radix-ui/react-dropdown-menu";
+import React from "react";
 
 const Header = () => {
   const { toggleSidebar } = useSidebar();
-  const { user } = UseAuth();
+  const { user, logout } = UseAuth();
+  const navigate = useNavigate()
 
-  console.log(user)
+  const handleLogout = async(e: React.MouseEvent<HTMLButtonElement>)=>{
+    e.preventDefault();
+    const logoutSuccess : unknown = await logout();
+    if(logoutSuccess){
+      navigate("/login")
+    }
+  }
+
   return (
     <nav className="p-4 sticky top-0 flex justify-between items-center z-20 backdrop-blur-md rounded-md">
       <div className="flex items-center gap-1">
@@ -21,10 +39,38 @@ const Header = () => {
         <Logo />
       </div>
       {user ? (
-        <Avatar className="mr-4 size-9">
-          <AvatarImage className="object-cover object-top cursor-pointer" src={user?.avatar} alt={user?.fullName} />
-          <AvatarFallback>{user?.fullName[0]}</AvatarFallback>
-        </Avatar>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="border-none outline-none">
+            <Avatar className="mr-4 size-9">
+              <AvatarImage
+                className="object-cover object-top cursor-pointer"
+                src={user?.avatar}
+                alt={user?.fullName}
+              />
+              <AvatarFallback>{user?.fullName[0]}</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-20 md:w-56 bg-black text-white border border-primary md:-mt-10 mr-7 md:mr-20 shadow-2xl">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <User />
+                <span>Profile</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <LayoutDashboard />
+                <span>Dashbaord</span>
+              </DropdownMenuItem>
+              <button onClick={handleLogout} className="w-full">
+                <DropdownMenuItem>
+                  <Power />
+                  <span>Logout</span>
+                </DropdownMenuItem>
+              </button>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       ) : (
         <Link to="/login">
           <Button variant="login">
